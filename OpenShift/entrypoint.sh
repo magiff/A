009979e8bd2d1de4abf58f479b7abb3b39b680ea
magiff <44117524+magiff@users.noreply.github.com>
2018-10-14 13:23:19 +0800

    Update entrypoint.sh

--- OpenShift/entrypoint.sh
+++ OpenShift/entrypoint.sh
@@ -1,9 +1,13 @@
-cd /v2raybin
 #echo -e "$CONFIG_JSON" > config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo -e "$CERT_PEM" > cert.pem
-  echo -e "$KEY_PEM"  > key.pem
-fi
+#if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+#  echo -e "$CERT_PEM" > cert.pem
+#  echo -e "$KEY_PEM"  > key.pem
+#fi
+
+cd /user/bin/v2ray
+wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+unzip v2ray.zip
+rm -rf v2ray.zip
 
 chmod +x v2ray v2ctl
 ./v2ray
