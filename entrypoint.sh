0751fe76961a01198536ac35f049ec2d4d015d72
magiff <44117524+magiff@users.noreply.github.com>
2018-10-14 12:48:18 +0800

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-cd /v2raybin
-#echo -e "$CONFIG_JSON" > config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo -e "$CERT_PEM" > cert.pem
-  echo -e "$KEY_PEM"  > key.pem
-fi
-
-chmod +x v2ray v2ctl
-./v2ray
