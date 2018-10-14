aa38643c815f94cf2e164eaaaafd87a63f966201
magiff <44117524+magiff@users.noreply.github.com>
2018-10-14 13:28:02 +0800

    Update entrypoint.sh

--- OpenShift/entrypoint.sh
+++ OpenShift/entrypoint.sh
@@ -4,7 +4,7 @@
 #  echo -e "$KEY_PEM"  > key.pem
 #fi
 
-cd /user/bin/v2ray
+cd /user/v2ray
 wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray.zip
 rm -rf v2ray.zip
